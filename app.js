8ad5dfd9c3ea070ce45a5bab81d44d2275091c20
doug42069 <mancuso220@gmail.com>
2025-09-17 14:24:27 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,21 +1,15 @@
-var redirect_uri = "https://doug42069.github.io/capstone2025NM/"
+const redirect_uri = "https://doug42069.github.io/capstone2025NM/";
 
-var client_id = "bd53535497384e2192f495522d3f3274";
-var client_secret = "bc864d2081654a88bc7e175fef7e0a14";
+const client_id = "bd53535497384e2192f495522d3f3274";
 
-const AUTHORIZE = "https://accounts.spotify.com/authorize"
+const AUTHORIZE = "https://accounts.spotify.com/authorize";
 
-function onPageLoad() {
-
-}
-
-
-function requestAuth() {
+function requestAuthorization() {
     let url = AUTHORIZE;
     url += "?client_id=" + client_id;
     url += "&response_type=code";
-    url += "&redirect_uri=" + encodeURI(redirect_uri);
-    url += "&showdialog=true";
+    url += "&redirect_uri=" + encodeURIComponent(redirect_uri);
+    url += "&show_dialog=true";
     url += "&scope=user-read-private user-read-email user-library-modify user-library-read playlist-read-private playlist-read-collaborative playlist-modify-private playlist-modify-public ugc-image-upload user-read-playback-state user-modify-playback-state user-read-currently-playing";
     window.location.href = url;
 }
